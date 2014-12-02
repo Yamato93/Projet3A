eed0729570519d758afd613c8f2fe81b53e54a1d
Thierry Khauv <naruto_kirby@hotmail.fr>
2014-12-02 11:15:42 +0100

    creat relog function

--- backoffice/app/view/login/logpage.view.php
+++ backoffice/app/view/login/logpage.view.php
@@ -31,7 +31,11 @@
         <h2 class="form-signin-heading">Please sign in</h2>
         <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
         <input type="password" name="pwd" class="form-control" placeholder="Password" required>
-        
+		<label class="checkbox">
+          <input type="checkbox" name="rememberme" value="remember-me"> Remember me
+        </label>
+
+
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
