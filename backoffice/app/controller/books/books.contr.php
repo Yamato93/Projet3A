155b36096eca98e36991c4996e2136dffdcd37f1
Thierry Khauv <naruto_kirby@hotmail.fr>
2014-12-08 02:08:54 +0100

    little change

---
+++ backoffice/app/controller/books/books.contr.php
@@ -0,0 +1,3 @@
+<?php
+
+include_once("../app/view/index/books.view.php");
--- backoffice/app/include/footer.inc.php
+++ backoffice/app/include/footer.inc.php
@@ -1,16 +1,10 @@
-	<div id="footer">
-		<p class="center">
-		Copyright (c) <span id="copyright">????</span> <?php echo SITE_NAME ?>. Exercice de formation. Design by 
-		<script type="text/javascript">
-		<!--
-			document.write(mail("champion", "gmail", "com", "Webdesigner"));
-		//-->
-		</script>. /
-		<a href="../backoffice/index.php">Se connecter</a>
-		<br/>
-		<span id="date">jjjjjj JJ mmmmmm AAAA</span> à <span id="heure">HH:MM:SS</span><br />
-		Page chargée en <span id="duree">????</span> ms
-		</p>
-	</div>
-<!-- Fin du container -->
-</div>
+ </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../www/public/js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="../www/public/js/evt_supp.js"></script>
+  </body>
+</html>
