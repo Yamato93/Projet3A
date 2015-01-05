0cba2b9a7e2918c33d803a4b3dd3d0b3973c7e0c
Thierry Khauv <naruto_kirby@hotmail.fr>
2015-01-05 13:15:07 +0100

    edit login controller
    put isset function for $_POST

--- app/controller/login/login.contr.php
+++ app/controller/login/login.contr.php
@@ -1,5 +1,5 @@
 <?php
-if ($_POST['login'] && $_POST['mail'] && $_POST['password']) {
+if (isset($_POST['login']) && isset($_POST['mail']) && isset($_POST['password'])) {
 	if (!empty($_POST['mail']) && !empty($_POST['password'])) {
 		$mail = $_POST['mail'];
 		$password = $_POST['password'];
@@ -14,21 +14,26 @@ if ($_POST['login'] && $_POST['mail'] && $_POST['password']) {
 	}
 }
 
-if ($_POST['register'] && $_POST['mail'] && $_POST['password'] && $_POST['passwordconfirm']) {
+if (isset($_POST['register']) && isset($_POST['mail']) && isset($_POST['password']) && isset($_POST['passwordconfirm'])){
 	if (!empty($_POST['mail']) && !empty($_POST['password']) && !empty($_POST['passwordconfirm']) ) {
 		$mail = $_POST['mail'];
 		$password = $_POST['password'];
 		$passwordconfirm = $_POST['passwordconfirm'];
-		if ($passwordconfirm == $password) {
+		if ($passwordconfirm == $password) 
+		{
 			include_once '../model/insert_new_user.model.php';
 			$password = md5($password);
-			if(insert_new_user($connect, $mail, $password)){
+			if(insert_new_user($connect, $mail, $password))
+			{
 				//header(string);
 			}
-			else{
+			else
+			{
 				die('erreur lors de l\'enregistrement');
 			}
-		else {
+		}
+		else 
+		{
 			die('password is different of the confirmation');
 		}
 	}
