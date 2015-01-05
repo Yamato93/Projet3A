954d455720ff4332a688a25e864161e5eb400fe5
Thierry Khauv <naruto_kirby@hotmail.fr>
2015-01-05 15:11:27 +0100

    Login v2.3.1

--- app/controller/login/login.contr.php
+++ app/controller/login/login.contr.php
@@ -1,26 +1,27 @@
 <?php
-if ($_POST['type']=='login' && isset($_POST['mail']) && isset($_POST['password'])) {
-	if (!empty($_POST['mail']) && !empty($_POST['password'])) {
-		$mail = $_POST['mail'];
-		$password = md5($_POST['password']);
-		include_once '../model/login/select_user_by_mail_password.model.php';
-		if (select_user_by_mail_password($connect, $mail, $password)) {
-			echo "user trouvé";
+if (isset($_POST['type'])=='login' && isset($_POST['mail']) && isset($_POST['password'])) {
+	
+		if (!empty($_POST['mail']) && !empty($_POST['password'])) {
+			$mail = $_POST['mail'];
+			$password = md5($_POST['password']);
+			include_once '../app/model/login/select_user_by_mail_password.model.php';
+			if (select_user_by_mail_password($connect, $mail, $password)) {
+				echo "user trouvé";
+			}
 		}
-	}
-	else{
-		die('mail and/or password empty');
-	}
+		else{
+			die('mail and/or password empty');
+		}
 }
 
-if ($_POST['type']=='register' && isset($_POST['mail']) && isset($_POST['password']) && isset($_POST['passwordconfirm'])){
+if (isset($_POST['type'])=='register' && isset($_POST['mail']) && isset($_POST['password']) && isset($_POST['passwordconfirm'])){
 	if (!empty($_POST['mail']) && !empty($_POST['password']) && !empty($_POST['passwordconfirm']) ) {
 		$mail = $_POST['mail'];
 		$password = $_POST['password'];
 		$passwordconfirm = $_POST['passwordconfirm'];
 
 		if ($passwordconfirm == $password) {
-			include_once '../model/login/insert_new_user.model.php';
+			include_once '../app/model/login/insert_new_user.model.php';
 
 			$password = md5($password);
 			if(insert_new_user($connect, $mail, $password))
