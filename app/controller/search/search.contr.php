77b033dc8c40a682db24b9eee9369c6a0090e260
AlexandreDpt <dpt.alexandre@gmail.com>
2015-01-27 16:48:17 +0100

    Search model is ready

--- app/controller/search/search.contr.php
+++ app/controller/search/search.contr.php
@@ -2,12 +2,16 @@
 if (isset($_POST['search'])) {
     include_once '../app/model/search/search.model.php';
     $search = '%'.$_POST['search'].'%';
-    //$search = '%Crotian%';
     $limit = 30;
     $offset = 0;
-    //echo $limit.'//'.$offset.'//'.$search;
     $data = search($connect, $search, $limit, $offset);
-    //var_dump($data);
+    if (!empty($data)) {
+        var_dump($data);
+        //include_once '../app/view/search/NOTNAMED.view.php'
+    }
+    else{
+        echo "no result";
+    }
 } else {
     header('location : index.php');
 }
--- app/model/search/search.model.php
+++ app/model/search/search.model.php
@@ -9,23 +9,27 @@ function search($connect, $search, $limit, $offset){
     try
     {
         $query ='SELECT *
-                FROM DT_BOOKS A, DT_BOOKS_STEPS B
-                WHERE A.BOOKS_ID = B.BOOKS_ID
-                AND ( BOOKS_TITLE LIKE :search
+                FROM DT_BOOKS A
+                LEFT JOIN DT_BOOKS_STEPS B
+                ON A.BOOKS_ID=B.BOOKS_ID
+                WHERE BOOKS_TITLE LIKE :search
                 OR BOOKS_DESCR LIKE :search
-                OR BOOKS_STEPS_CONTENT LIKE :search )
+                OR BOOKS_STEPS_CONTENT LIKE :search
                 LIMIT :offset, :limit ;' ;
+
         $curseur = $connect->prepare($query);
+
         $curseur ->bindValue(':search', $search, PDO::PARAM_STR);
         $curseur ->bindValue(':offset', $offset, PDO::PARAM_INT);
         $curseur ->bindValue(':limit', $limit, PDO::PARAM_INT);
 
         $curseur->execute();
-        var_dump($curseur);
+
         $data_user = $curseur -> fetchAll(PDO::FETCH_OBJ);
-        var_dump($data_user);
+
         $curseur->closeCursor();
         if(isset($data_user))
+
         {
             return current($data_user);
         }
