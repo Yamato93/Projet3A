9eb802b23b430505ec994f17b2f7ce53bc37e2b4
AlexandreDpt <dpt.alexandre@gmail.com>
2015-01-29 13:21:28 +0100

    Add Facebook SDK php

--- app/controller/sample/sample.contr.php
+++ app/controller/sample/sample.contr.php
@@ -1,44 +1,8 @@
 <?php
 /**
- *Controller for ajax request
- *
+ * Created by PhpStorm.
+ * User: alexandredupont
+ * Date: 29/01/15
+ * Time: 11:15
  */
-	include_once("../app/model/connection/verif_user_login.php");
-	
-	$valeur = sign_in($connect, $_POST["login"], $_POST["password"]);
-	
-	if($valeur == 1)
-	{	
-		
-		//$_SESSION["User_connect"] = true;
-		//$_SESSION["User"] = $valeur["User_log"];
-		if (!setcookie(COOKIE_LOG, $_POST["login"],time()+3600*24*31))
-		{
-			die("Cookie can't be saved !");
-		}
-		if (!setcookie(COOKIE_PASS, $_POST["password"], time()+3600*24*31))
-		{
-			die("Cookie can't be saved !");
-		}
-		$_SESSION["User_connect"] = true;
-		$answer = array( 
-						"Nombre user" =>  $valeur,
-						"Message" => "Known user");
-	}
-	else if ($valeur == 0)
-	{
-		$_SESSION["User_connect"] = false;
-		$answer = array( 
-						"Nombre user" =>  $valeur,
-						"Message" => "Unknown user");
-	}
-	else
-	{
-		$_SESSION["User_connect"] = false;
-		$answer = array( 
-						"Nombre user" => $valeur,
-						"Message" => "To much user");
-	}
-	
-	echo json_encode($answer);
-?>
+include_once('../app/view/sample/sample.view.php');
