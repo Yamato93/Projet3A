ea53c9722978f32b79c775ad6fc3f4bd2448ed7a
AlexandreDpt <dpt.alexandre@gmail.com>
2015-02-05 09:25:01 +0100

    New Facebook connect

--- app/model/users/select_user_by_mail_fbid.model.php
+++ app/model/users/select_user_by_mail_fbid.model.php
@@ -11,12 +11,13 @@ function select_user_by_mail_fbid($connect, $mail, $fb_id)
 	try
 	{
 		$query ="SELECT * FROM DT_USERS
-				WHERE USE_MAIL = :mail";
+				WHERE USE_MAIL = :mail
+				AND USE_FACEBOOK_ID = :fb_id ";
 		//OR USE_FB_ID = :fb_id
 							
 		$curseur = $connect->prepare($query); 
 		$curseur ->bindValue(':mail', $mail, PDO::PARAM_STR);
-		//$curseur ->bindValue(':fb_id', $fb_id, PDO::PARAM_STR);
+		$curseur ->bindValue(':fb_id', $fb_id, PDO::PARAM_INT);
 		$curseur->execute();
 		$data_user = $curseur -> fetchAll(PDO::FETCH_OBJ);
 		$curseur->closeCursor();
