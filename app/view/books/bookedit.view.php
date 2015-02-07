932ed2b69e1c872837ca8e70d2f014a0f730531a
Gaetan Boedec <gboedec@orange.fr>
2015-02-07 14:23:55 +0100

    fix edit memorie

--- app/view/books/bookedit.view.php
+++ app/view/books/bookedit.view.php
@@ -10,18 +10,18 @@
         <!-- END HEADER -->
 
         <!-- CONTENT -->
-        <div class="book-edit-container">
+        <div class="memorie-container">
 			
             <?php include('../app/includes/nav.inc.php') ?>
 			<?php foreach ($bookslist as $key => $value)
 			{
 			?>
             <div class="journal-container u-wrapper">
-                <form action="index.php?module=books&action=updatebookscover&bookid=<?php echo $_GET['id'];?>" method="post" enctype="multipart/form-data">
+                <form class="grid" action="index.php?module=books&action=updatebookscover&bookid=<?php echo $_GET['id'];?>" method="post" enctype="multipart/form-data">
                     <h1>Let's the journey begin</h1>
                     <h2 class="sub-title">Edit your travel journal to enrich your travel journal</h2>
-                    <div class="grid">
-                        <div class="column-half" style="background-image:url(<?php echo $value->BOOKS_COVER;?>);">
+                    <div class="column-half cover-journal">
+                        <div style="background-image:url(<?php echo $value->BOOKS_COVER;?>);">
                             <h2 class="cover-title">
                                 <input type="text" name="cover_title" placeholder="Title" value="<?php echo $value->BOOKS_TITLE;?>"/>
                             </h2>
@@ -30,56 +30,44 @@
                                 <input type="file" name="cover_img" id="cover"/>
                             </span>
                         </div>
-                        <div class="column-half">
+                        <div>
                             <textarea placeholder="Your description" value="<?php echo $value->BOOKS_DESCR;?>" name="cover_descr" ><?php echo $value->BOOKS_DESCR;?></textarea>
                         </div>
                     </div>
-                    <div class="tab-container">
-                        <ul>
-                            <li>
-                                <a href="#memories" class="active">Memories</a>
-                            </li>
-                            <li>
-                                <a href="#map">Map</a>
-                            </li>
-                        </ul>
-                        <div id="memories" class="tab-content active">
-                        	<ul>
-								<?php
-									foreach ($booksstep as $key => $value)
-								    {
-								    	$passage = false;
-										foreach ($booksstepimg as $keys => $values)
-										{
-										   //commandes
-											if($passage == false)
-											{
-												if($value->BOOKS_STEPS_ID == $values->BOOKS_STEPS)
-												{
-												$passage = true;
-												$url = $values->STEPS_IMG;
-												}
-											}
-											
-										}
-										
-										?>
-		                                <li>
-		                                    <img src="<?php echo $url; ?>" alt="Preview image form empty">
-		                                    <a href="index.php?module=books&action=updatebookstory&bookid=<?php echo $_GET['id']; ?>&info=<?php echo $value->BOOKS_STEPS_ID; ?>"></a>
-		                                </li>
+                    <div class="column-half memorie">
+                            <ul class="grid">
+                                <?php
+                                    foreach ($booksstep as $key => $value)
+                                    {
+                                        $passage = false;
+                                        foreach ($booksstepimg as $keys => $values)
+                                        {
+                                           //commandes
+                                            if($passage == false)
+                                            {
+                                                if($value->BOOKS_STEPS_ID == $values->BOOKS_STEPS)
+                                                {
+                                                $passage = true;
+                                                $url = $values->STEPS_IMG;
+                                                }
+                                            }
+                                            
+                                        }
+                                        
+                                        ?>
+                                        <li class="column-half">
+                                            <img src="<?php echo $url; ?>" alt="Preview image form empty">
+                                            <a href="index.php?module=books&action=updatebookstory&bookid=<?php echo $_GET['id']; ?>&info=<?php echo $value->BOOKS_STEPS_ID; ?>"></a>
+                                        </li>
                                <?php
                                     }   
                                 ?>
-                                <li>
+                                <li class="column-half">
                                     <img src="public/img/preview.jpg" alt="Preview image form empty">
                                     <a href="index.php?module=books&action=bookseditstory&id=<?php echo $_GET['id']; ?>">+</a>
                                 </li>
                             </ul>
-                        </div>
-                        <div id="map" class="tab-content">
-                        </div>
-                    </div>
+                        </div
                     <button class="save-journal" type="submit">Save your books</button>
                 </form>
             </div>
