28979a00bdb027381e2b5dee597f1fe96e2f8b34
Thierry Khauv <naruto_kirby@hotmail.fr>
2015-03-01 15:28:24 +0100

    Creat / Delete / Edit Books without Test Done !!

--- app/controller/books/delete_books.contr.php
+++ app/controller/books/delete_books.contr.php
@@ -1,16 +1,18 @@
 <?php
 include("../core/function/function_delete.php");
 include_once("../app/model/books/select_data_books.php");
-echo "<pre>";
-print_r($_GET['id']);
-echo "<pre>";
+
 if(select_data_books($connect, BASE, "DT_BOOKS", "BOOKS_ID" , $_GET['id'], "USE_ID", $_SESSION['User']->USE_ID))
 {
 	if(delete($connect, BASE, "DT_BOOKS", "BOOKS_ID", $_GET['id']))
 	{
+		header('Location:index.php?module=books&action=bookslist&message=deleteok');
+		exit();
 	}
 	else
 	{
+		header('Location:index.php?module=books&action=bookslist&message=deletefail');
+		exit();
 	}
 }
 
--- app/controller/books/updatebookstory.contr.php
+++ app/controller/books/updatebookstory.contr.php
@@ -9,30 +9,37 @@ if(isset($_SESSION['User']))
 {	
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{	   
-
+		
 		if(update_book_story($connect, BASE, "DT_BOOKS_STEPS", $_SESSION['User']->USE_ID, $_GET['bookid'], $_GET['info'], $_POST['story'], $_POST['start-date'], $_POST['end-date']))
 		{
 			if(isset($_FILES))
 			{
-				$i = 1;
-				$y = 1;
-				while(isset($_FILES['story-img'.$i]))
+				$countfile = count($_FILES);
+				$z=1;
+				for ($i = 1; $i <= $countfile; $i++) 
 				{
-									
-					if($_FILES['story-img'.$i]['error'] == 0)
+				    if((isset($_FILES['story-img'.$i])) and ($_FILES['story-img'.$i]['name'] != ''))
+				    {
+						if($_FILES['story-img'.$i]['error'] == 0)
+						{
+							upload('story-img'.$i,'Updatebookstory', 'step_img'.$z);
+						} 
+						$z++;
+				    }
+				}
+				
+				$countfilesession = count($_SESSION['Updatebookstory']);
+				for ($y = 1; $y <= $countfilesession; $y++) 
+				{
+					if(isset($_SESSION['Updatebookstory']['step_img'.$y]))
 					{
-						upload('story-img'.$i,'Updatebookstory', 'step_img'.$i);
+						insert_step_img($connect, BASE, "DT_BOOKS_STEPS_PICTURE", $_GET['info'], $_GET['bookid'], $_SESSION['User']->USE_ID, $_SESSION['Updatebookstory']['step_img'.$y]);
 					}
-					$i++;
+
 				}
-				while(isset($_SESSION['Updatebookstory']['step_img'.$y]))
-				{
-					insert_step_img($connect, BASE, "DT_BOOKS_STEPS_PICTURE", $_GET['info'], $_GET['bookid'], $_SESSION['User']->USE_ID, $_SESSION['Updatebookstory']['step_img'.$y]);
-					$y++;
-				}
-				//unset($_SESSION['Updatebookstory']);
+				unset($_SESSION['Updatebookstory']);
 			}
-			header('location:index.php?module=books&action=bookslist&message=editok');
+			header('location:index.php?module=books&action=bookedit&id='.$_GET['bookid']);
 			exit;
 		}
 		else
