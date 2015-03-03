da281a541cce4f28bf5cf38031f7cf551ef1f53b
Thierry Khauv <naruto_kirby@hotmail.fr>
2015-03-03 22:39:39 +0100

    edit delete img

--- app/controller/books/delete_steps_img.contr.php
+++ app/controller/books/delete_steps_img.contr.php
@@ -2,19 +2,17 @@
 include("../core/function/function_delete.php");
 include_once("../app/model/books/select_data_steps.php");
 
-if(select_data_step_info($connect, BASE, "DT_BOOKS_STEPS", "BOOKS_STEPS_ID",$_GET['info'], "BOOKS_ID", $_GET['bookid'], "USE_ID", $_SESSION['User']->USE_ID))
+if(select_data_step_info($connect, BASE, "DT_BOOKS_STEPS", "BOOKS_STEPS_ID", $_GET['info'], "BOOKS_ID", $_GET['bookid'], "USE_ID", $_SESSION['User']->USE_ID))
 {
 	if(delete($connect, BASE, "DT_BOOKS_STEPS_PICTURE", "PIC_ID", $_GET['img_id']))
 	{
 		sessionize('success','Your picture has been delete');
-		header('Location:index.php?module=books&action=bookslist&message=deleteok');
-		exit();
+		return true;
 	}
 	else
 	{
 		sessionize('danger','Your picture has not been delete');
-		header('Location:index.php?module=books&action=bookslist&message=deletefail');
-		exit();
+		return false;
 	}
 }
 else
