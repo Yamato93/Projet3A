d68c82deb07ca13e1333716c836a345262fb2d53
Gaetan Boedec <gboedec@orange.fr>
2015-03-04 10:34:55 +0100

    Fix error

--- app/controller/books/delete_books_steps.contr.php
+++ app/controller/books/delete_books_steps.contr.php
@@ -2,7 +2,7 @@
 include("../core/function/function_delete.php");
 include_once("../app/model/books/select_data_steps.php");
 
-if(select_data_step_picture($connect, BASE, "DT_BOOKS_STEPS_PICTURE", "USE_ID", $_SESSION['User']->USE_ID, "BOOKS_ID", $_GET['bookid'], "BOOKS_STEPS", $_GET['info']);)
+if(select_data_step_picture($connect, BASE, "DT_BOOKS_STEPS_PICTURE", "USE_ID", $_SESSION['User']->USE_ID, "BOOKS_ID", $_GET['bookid'], "BOOKS_STEPS", $_GET['info']))
 {
 	if(delete($connect, BASE, "DT_BOOKS_STEPS", "BOOKS_STEPS_ID", $_GET['info']))
 	{
